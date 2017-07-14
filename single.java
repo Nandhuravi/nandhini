5f68a1d2afa457ea4bf227caa3cb903ba8889c4b
Nandhuravi <rnandhiniksrcebtechit@gmail.com>
2017-07-14 11:07:23 +0530

    Create single.java

---
+++ single.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class single
+{
+
+   
+    public static void main(String[] args) 
+{
+      int n,an=0,b=0,t,i=0,t1;
+        
+        
+        Scanner s2=new Scanner(System.in);
+        n=s2.nextInt();
+        while(n>0)
+        {
+            t=n%10;
+            b=b*10+(t*t*t);
+            n=n/10;
+    }
+        if(b==n)
+        {
+            System.out.println("palindrome");
+        }
+        else
+
+        {
+                    System.out.println("not palindrome"); 
+        }
+    
+}}
+
+
