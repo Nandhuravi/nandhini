eb9a12148eb71f466a4f87cda21e0d068db5ec9c
Nandhuravi <rnandhiniksrcebtechit@gmail.com>
2017-07-14 10:35:40 +0530

    Create unsort.java

---
+++ unsort.java
@@ -0,0 +1,20 @@
+import java.util.*;
+	public class unsort{
+
+	     public static void main(String []args){
+	        Scanner s=new Scanner(System.in);
+	        int n=s.nextInt();
+	        int ar[]=new int[n];
+	        for(int i=0;i<n;i++)
+	            ar[i]=s.nextInt();
+	        Set<Integer> set = new HashSet<Integer>();
+	        for(int i=0;i<n;i++){
+	            if(set.contains(ar[i])){
+	                System.out.println(ar[i]);
+	                break;}
+	            else
+	                set.add(ar[i]);
+	        }
+	       
+	     }
+	}
