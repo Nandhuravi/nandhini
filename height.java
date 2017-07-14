2372ef195e7456ca865038655b3edd065fe4eba1
Nandhuravi <rnandhiniksrcebtechit@gmail.com>
2017-07-14 11:03:15 +0530

    Create height.java

---
+++ height.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+	import java.util.Scanner;
+
+	public class height{
+	    public static void main(String ar[])
+	    {
+			Scanner s=new Scanner(System.in);
+			int arr[]=new int[5];
+			for(int i=0;i<arr.length;i++)
+			{
+				arr[i]=s.nextInt();
+			}
+			Arrays.sort(arr);
+			System.out.println("enter the position : ");
+			int pos=s.nextInt();
+			System.out.println(arr.length-pos);
+			s.close();
+		}
+	    
+	}
