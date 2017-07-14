29b9edf0a59266d351b02af97c30b74df879af7e
Nandhuravi <rnandhiniksrcebtechit@gmail.com>
2017-07-14 10:48:14 +0530

    Create nearest.java

---
+++ nearest.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+	public class nearest {
+
+	public static void main(String[] args) {
+	
+		Scanner qq = new Scanner(System.in);
+		System.out.println("enter size of array");
+		int nn = qq.nextInt();
+		int count[] = new int[nn];
+		System.out.println("enter array element");
+		for (int i = 0; i < nn; i++) {
+			count[i] = qq.nextInt();
+		}
+		int sum;
+		int g = 0, n = 0, h = 0;
+		for (int i = 0; i < nn; i++) {
+			for (int j = i + 1; j < nn; j++) {
+				sum = 0;
+				sum = count[i] + count[j];
+				if (sum < 0) {
+					sum = sum * -1;
+				}
+				if (i == 0 && j == 1) {
+					n = sum;
+					g = i;
+					h = j;
+					break;
+				}
+				if (sum < n) {
+					n = sum;
+					g = i;
+					h = j;
+					break;
+				}
+			}
+		}
+		System.out.println("The two elements are " + count[g] + " and "
+				+ count[h]);
+
+	}
+
+	}
+
