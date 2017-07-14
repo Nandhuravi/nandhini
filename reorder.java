b94cea5998112ae8779d3b43175a249a6cb446a5
Nandhuravi <rnandhiniksrcebtechit@gmail.com>
2017-07-14 11:10:25 +0530

    Create reorder.java

---
+++ reorder.java
@@ -0,0 +1,17 @@
+import java.util.*;
+	public class reorder {
+		public static void main(String args[])
+		{
+		Scanner ss=new Scanner(System.in);
+		int n=ss.nextInt();
+		ArrayList<Integer> al=new ArrayList<Integer>();
+		for(int i=0;i<n;i++)
+		{
+		al.add(ss.nextInt());
+		}
+		Collections.shuffle(al);
+		System.out.println(al);
+		}
+	}
+
+
