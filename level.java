bf00cc69ca8cc9661abe7dfaf5aad95246e21cb5
Nandhuravi <rnandhiniksrcebtechit@gmail.com>
2017-07-14 11:32:25 +0530

    Create level.java

---
+++ level.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class level {
+
+    
+    public static void main(String[] args) {
+       
+        System.out.println("enter the no");
+        Scanner in=new Scanner(System.in);
+        int n= in.nextInt();
+         if((100-n)<=10)
+             {
+             System.out.println("true");
+        if((200-n)<=10){
+             System.out.println("true");
+        }else
+                     System.out.println("sakdsajhd");
+            }else
+             System.out.println(" ........");
+    }
+    }
+    
