3502621898495ffe0b797a21d1c44475f43a2adf
Nandhuravi <rnandhiniksrcebtechit@gmail.com>
2017-07-14 15:23:07 +0530

    Create contiguous.java

---
+++ contiguous.java
@@ -0,0 +1,18 @@
+public class contiguous {
+	
+	public int maxSubArray(int[] a){
+		 
+	       int b=a[0];
+	       int max=a[0];
+	       for(int i=1;i<a.length;i++)
+	       {
+	           b=Math.max(b+a[i],a[i]);
+	           max= Math.max(max, b);
+	       }
+	       return max;
+	    }
+	public static void main(String[] args) {
+		contiguous m=new contiguous();
+		 int[] a=new int[]{1,2,3,4,5,6,7};
+		m.maxSubArray(a);
+	}}
