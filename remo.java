1b12047925ab9bda7f1f0298609be504b2313588
Nandhuravi <rnandhiniksrcebtechit@gmail.com>
2017-07-17 17:30:49 +1300

    Create remo.java

---
+++ remo.java
@@ -0,0 +1,39 @@
+import java.util.*;
+	import java.io.*;
+	public class remo
+	{
+	  public static void main(String[] args)
+	  {
+	    System.out.println("ENTER THE STRING");
+	    Scanner s=new Scanner(System.in);
+	    String original=s.nextLine();
+	        for(int i=0;i<original.length();i++)
+	    {  
+	        	String temp="";
+	       for(int j=0;j<original.length();j++)
+	       {
+	         if(i!=j)
+	         {
+	           temp=temp+original.charAt(j);
+	         }
+	       }
+	       
+	       String reverse=rev(temp);
+	       if(temp.equals(reverse))
+	         {
+	          System.out.println("AFTER REMOVING << "+original.charAt(i)+" >> THE STRING << "+original+" >> BECOMES << "+temp+" >> THAT IS PALINDROME\n");
+	         }
+	     }
+	  }
+	  static String rev(String r)
+	   {
+	      String x="";
+	    for(int i=(r.length())-1;i>=0;i--)
+	     {
+	        x=x+r.charAt(i);
+	     }
+	    return x;
+	   } 
+	}
+
+
