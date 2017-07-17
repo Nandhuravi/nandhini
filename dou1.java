4f9dbadd01569cd12e320ea43088b51b2f374418
Nandhuravi <rnandhiniksrcebtechit@gmail.com>
2017-07-17 17:12:32 +1300

    Create dou1.java

---
+++ dou1.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class dou1 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the string");
+		Scanner input=new Scanner(System.in);
+		String str=input.nextLine();
+		input.close();
+		if(str.length()%2==0)
+		{
+			String str1=str.substring(0,str.length()/2);
+			String str2=str.substring(str.length()/2,str.length());
+			if(str1.equals(str2))
+			{
+				System.out.println("The given string is  repeat");
+			}
+			else
+			{
+				System.out.println("The given string is not repeat");
+			}
+		}
+		if(str.length()%2!=0)
+		{
+			String str1=str.substring(0,str.length()/2);
+			String str2=str.substring(str.length()/2+1,str.length());
+			if(str1.equals(str2))
+			{
+				System.out.println("The given string is  repeat");
+			}
+			else
+			{
+				System.out.println("The given strinng is not repeat");
+			}
+		}
+
+	}
+
+}
