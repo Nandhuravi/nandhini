374a8d5f49f5f8c00543648d5c1abba6e6f988e7
Nandhuravi <rnandhiniksrcebtechit@gmail.com>
2017-07-17 19:05:19 +1300

    Create combi.java

---
+++ combi.java
@@ -0,0 +1,19 @@
+import java.util.*;
+	public class combi{
+
+	    public static void main(String[] args) {
+
+	       Scanner in=new Scanner(System.in);
+	 int n=in.nextInt();
+	 int k=in.nextInt();
+	 for(int i=1;i<=n;i++)
+	    {
+	        for(int j=i+1;j<=n;j++)
+	        {
+	            System.out.println(i+""+j);        
+	        }
+	 
+	    }
+	}
+	    
+	}
