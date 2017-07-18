c2935831e52fd30c34487552ea49a45a6623549f
Nandhuravi <rnandhiniksrcebtechit@gmail.com>
2017-07-18 17:13:50 +1300

    Create Set1.java

---
+++ Set1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+	public class Set1{
+	public static void main(String dd[]){
+		String s;		
+		Scanner sc=new Scanner(System.in);
+		s=sc.next();
+		String b[]=new String[11];
+	    int k=0,count=0;
+
+		
+			for(int j=1;j<=s.length()-1;j++){
+				System.out.println("p"+s.charAt(j));
+			
+					}
+				
+			
+		
+		
+	}
+	}
+
+
